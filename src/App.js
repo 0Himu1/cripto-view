73fe7a0983ec4d9e1484987b45472d281960add1
0Himu1 <0himu1@gmail.com>
2022-02-13 18:22:28 +0600

    Leyout Design done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import { Layout } from "antd";
+import "antd/dist/antd.css";
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import {
+  CryptoCurrencies,
+  CryptoDetails,
+  Exchanges,
+  HomePage,
+  Navbar,
+  News,
+} from "./Components/index";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<CryptoCurrencies />} />
+              <Route path="/crypto/:coinid" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+      </div>
+      <div className="footer">Footer</div>
+    </div>
+  );
 }
 
 export default App;
